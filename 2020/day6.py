1829a566553969c867f725d4bcb7caecd964d817
N473 RCL0U <faircln@stpaulsschool.org.uk>
2023-09-06 11:43:30 +0100

    Set Practice

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,12 +1,21 @@
+total = 0
+running_set = set()
+
 infile = open("day_6_input.txt", "r")
 line = infile.readline()
-set_letters = {'a'}
-total = 0
 while line:
     if line == '\n':
-        total += len(set_letters)
-        set_letters.clear()
+        total += len(running_set)
+        running_set.clear()
     else:
-        for letter in line.split(' '):
-            set_letters.add(letter)
+        for c in line.strip():
+            running_set.add(c)
+    line = infile.readline()
+
+total += len(running_set)
+infile.close()
+
+
 print(total)
+
+#6335
